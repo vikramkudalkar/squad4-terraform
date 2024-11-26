2ebd2ed0c562ebec591d3f2db1a5a53c78e67797
vikramkudalkar <100548486+vikramkudalkar@users.noreply.github.com>
2024-11-26 16:20:28 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
-terraform{
-required_providers{
-aws={
-source = "hasicorp/aws"
-version = "5.56.1"
-}
-}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
 }
