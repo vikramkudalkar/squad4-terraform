1a6c521fb0aeffd22b93328fada830ff6bbd7867
vikramkudalkar <100548486+vikramkudalkar@users.noreply.github.com>
2024-11-26 15:58:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "squad4_vpc" {
 resource "aws_subnet" "squad4_public_subnet" {
   vpc_id    = aws_vpc.squad4_vpc.id
   cidr_block = "10.0.1.0/24"  
-  availability_zone = "us-west-2a"  
+  availability_zone = "us-east-1a"  
   map_public_ip_on_launch = true  
 
   tags = {
@@ -25,7 +25,7 @@ resource "aws_subnet" "squad4_public_subnet" {
 resource "aws_subnet" "squad4_private_subnet" {
   vpc_id    = aws_vpc.squad4_vpc.id
   cidr_block = "10.0.2.0/24"  
-  availability_zone = "us-west-2b"  
+  availability_zone = "us-east-1b"  
   map_public_ip_on_launch = true  
 
   tags = {
